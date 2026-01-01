b27f540bc02710516f43c8979c5d03e3f51afaf1
javaquery <vicky.thakor@javaquery.com>
2026-01-01 11:55:22 +0530

    Add S3Service class for AWS S3 operations including upload, download, and presigned URL generation

---
+++ module/spring-aws/src/main/java/com/javaquery/spring/aws/S3Service.java
@@ -0,0 +1,248 @@
+package com.javaquery.spring.aws;
+
+import com.javaquery.util.Is;
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import lombok.Builder;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+/**
+ * @author vicky.thakor
+ * @since 2026-01-01
+ */
+@Component
+public class S3Service {
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3Service.class);
+
+    @Value("${aws.signature.duration:5}")
+    private int signatureDuration;
+
+    private final AwsCredentialsProvider awsCredentialsProvider;
+
+    public S3Service(AwsCredentialsProvider awsCredentialsProvider) {
+        this.awsCredentialsProvider = awsCredentialsProvider;
+    }
+
+    /**
+     * Generate a presigned URL for uploading an object to S3.
+     *
+     * @param region     The AWS region where the S3 bucket is located.
+     * @param bucketName The name of the S3 bucket.
+     * @param objectKey  The key (path) of the object to be uploaded.
+     * @return A presigned URL as a String.
+     */
+    public String getPutPresignedUrl(Region region, String bucketName, String objectKey) {
+        try (S3Presigner preSigner = S3Presigner.builder()
+                .region(region)
+                .credentialsProvider(awsCredentialsProvider)
+                .build()) {
+            PutObjectRequest putObjectRequest =
+                    PutObjectRequest.builder().bucket(bucketName).key(objectKey).build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .putObjectRequest(putObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(signatureDuration))
+                    .build();
+
+            PresignedPutObjectRequest presignedRequest = preSigner.presignPutObject(presignRequest);
+            return presignedRequest.url().toString();
+        }
+    }
+
+    /**
+     * Generate a presigned URL for downloading an object from S3.
+     *
+     * @param region     The AWS region where the S3 bucket is located.
+     * @param bucketName The name of the S3 bucket.
+     * @param objectKey  The key (path) of the object to be downloaded.
+     * @return A presigned URL as a String.
+     */
+    public String getGetPresignedUrl(Region region, String bucketName, String objectKey) {
+        try (S3Presigner preSigner = S3Presigner.builder()
+                .region(region)
+                .credentialsProvider(awsCredentialsProvider)
+                .build()) {
+            GetObjectRequest getObjectRequest =
+                    GetObjectRequest.builder().bucket(bucketName).key(objectKey).build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .getObjectRequest(getObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(signatureDuration))
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = preSigner.presignGetObject(presignRequest);
+            return presignedRequest.url().toString();
+        }
+    }
+
+    /**
+     * Upload an object to S3 and return a presigned URL for downloading it.
+     *
+     * @param uploadObject The S3UploadObject containing upload details.
+     * @return A presigned URL as a String.
+     */
+    public String uploadObject(S3UploadObject uploadObject) {
+        try (S3AsyncClient s3AsyncClient = S3AsyncClient.builder()
+                .region(uploadObject.getRegion())
+                .credentialsProvider(awsCredentialsProvider)
+                .build()) {
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(uploadObject.getBucketName())
+                    .key(uploadObject.getDestination())
+                    .build();
+
+            CompletableFuture<PutObjectResponse> response = s3AsyncClient.putObject(
+                    putObjectRequest, Paths.get(uploadObject.getSource().getAbsolutePath()));
+
+            // Wait for upload to finish
+            response.join();
+
+            if (Is.nonNullNonEmpty(uploadObject.getTags())) {
+                Tagging tagging = Tagging.builder()
+                        .tagSet(uploadObject.getTags().entrySet().stream()
+                                .map(entry -> Tag.builder()
+                                        .key(entry.getKey())
+                                        .value(entry.getValue())
+                                        .build())
+                                .collect(Collectors.toList()))
+                        .build();
+                PutObjectTaggingRequest taggingRequest = PutObjectTaggingRequest.builder()
+                        .bucket(uploadObject.getBucketName())
+                        .key(uploadObject.getDestination())
+                        .tagging(tagging)
+                        .build();
+                s3AsyncClient.putObjectTagging(taggingRequest).join();
+            }
+            return getGetPresignedUrl(
+                    uploadObject.getRegion(), uploadObject.getBucketName(), uploadObject.getDestination());
+        }
+    }
+
+    /**
+     * Delete an object from S3.
+     *
+     * @param region     The AWS region where the S3 bucket is located.
+     * @param bucketName The name of the S3 bucket.
+     * @param objectKey  The key (path) of the object to be deleted.
+     */
+    public void deleteObject(Region region, String bucketName, String objectKey) {
+        try (S3AsyncClient s3AsyncClient = S3AsyncClient.builder()
+                .region(region)
+                .credentialsProvider(awsCredentialsProvider)
+                .build()) {
+
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .build();
+
+            CompletableFuture<DeleteObjectResponse> response = s3AsyncClient.deleteObject(deleteObjectRequest);
+
+            response.whenComplete((deleteObjectResponse, ex) -> {
+                if (deleteObjectResponse == null) {
+                    LOGGER.error(ex.getMessage(), ex);
+                }
+            });
+            response.thenApply(r -> null);
+        }
+    }
+
+    /**
+     * Copy an object from one S3 location to another.
+     *
+     * @param region                 The AWS region where the S3 buckets are located.
+     * @param sourceBucketName       The name of the source S3 bucket.
+     * @param sourceKey              The key (path) of the source object.
+     * @param destinationBucketName  The name of the destination S3 bucket.
+     * @param destinationKey         The key (path) for the copied object.
+     * @return A CompletableFuture containing the copy result as a String.
+     */
+    public CompletableFuture<String> copyObjectAsync(
+            Region region,
+            String sourceBucketName,
+            String sourceKey,
+            String destinationBucketName,
+            String destinationKey) {
+        try (S3AsyncClient s3AsyncClient = S3AsyncClient.builder()
+                .region(region)
+                .credentialsProvider(awsCredentialsProvider)
+                .build()) {
+
+            CopyObjectRequest copyObjectRequest = CopyObjectRequest.builder()
+                    .sourceBucket(sourceBucketName)
+                    .sourceKey(sourceKey)
+                    .destinationBucket(destinationBucketName)
+                    .destinationKey(destinationKey)
+                    .build();
+
+            CompletableFuture<CopyObjectResponse> response = s3AsyncClient.copyObject(copyObjectRequest);
+
+            response.whenComplete((copyObjectResponse, ex) -> {
+                if (copyObjectResponse == null) {
+                    LOGGER.error(ex.getMessage(), ex);
+                }
+            });
+            return response.thenApply(CopyObjectResponse::copyObjectResult).thenApply(Object::toString);
+        }
+    }
+
+    /**
+     * Move an object from one S3 location to another by copying and then deleting the source object.
+     *
+     * @param region                 The AWS region where the S3 buckets are located.
+     * @param sourceBucketName       The name of the source S3 bucket.
+     * @param sourceKey              The key (path) of the source object.
+     * @param destinationBucketName  The name of the destination S3 bucket.
+     * @param destinationKey         The key (path) for the moved object.
+     */
+    public void moveObject(
+            Region region,
+            String sourceBucketName,
+            String sourceKey,
+            String destinationBucketName,
+            String destinationKey) {
+        CompletableFuture<String> response =
+                copyObjectAsync(region, sourceBucketName, sourceKey, destinationBucketName, destinationKey);
+        response.whenComplete((copyRes, ex) -> {
+            if (copyRes != null) {
+                deleteObject(region, sourceBucketName, sourceKey);
+            } else {
+                LOGGER.error(ex.getMessage(), ex);
+            }
+        });
+        response.join();
+    }
+
+    /**
+     * S3 Upload Object details.
+     */
+    @Getter
+    @Builder
+    public static class S3UploadObject {
+        @Builder.Default
+        private Region region = Region.US_EAST_1;
+
+        private String bucketName;
+        private String destination;
+        private File source;
+        private Map<String, String> tags;
+    }
+}
